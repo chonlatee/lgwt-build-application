2a22abb2f107a36c5c81f067853d789af55db6ba
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-22 23:27:27 +0700

    finish Chapter IO and Sorting

--- main.go
+++ main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
-	store := NewInMemoryPlayerStore()
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+	store, err := NewFileSystemPlayerStore(db)
+	if err != nil {
+		log.Fatalf("problem creating file system player store, %v", err)
+	}
 	server := NewPlayerServer(store)
-	log.Fatal(http.ListenAndServe(":5000", server))
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
 }
